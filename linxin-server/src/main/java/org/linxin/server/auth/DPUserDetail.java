d036818fb0cb9ad01fb21790ce99cc8171a73b65
duzhihua <duzhihua@linxin.dev>
2026-04-01 11:14:10 +0800

    fix: make DPUserDetail implement UserDetails interface

--- linxin-server/src/main/java/org/linxin/server/auth/DPUserDetail.java
+++ linxin-server/src/main/java/org/linxin/server/auth/DPUserDetail.java
@@ -1,12 +1,36 @@
 package org.linxin.server.auth;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
-public class DPUserDetail {
+import java.util.Collection;
+import java.util.Collections;
 
-    private Long userId;
-    private String username;
-    private String password;
+@Getter
+@AllArgsConstructor
+public class DPUserDetail implements UserDetails {
 
+    private final Long userId;
+    private final String username;
+    private final String password;
+    private final Integer status;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return status != null && status == 1; }
 }
